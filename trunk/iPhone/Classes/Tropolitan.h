894bc263780f17ddc01757e8a7e3fdf55b31efe0
rmorris@stewonit.com <rmorris@stewonit.com@11dcd478-d5f8-5778-5ec9-787184ed5f8b>
2011-03-08 03:45:24 +0000

    Forgot to add project files

---
+++ trunk/iPhone/Classes/Tropolitan.h
@@ -0,0 +1,35 @@
+//
+//  Tropolitan.h
+//  iPhone
+//
+//  Created by HSJC Student Account on 3/1/11.
+//  Copyright 2011 Troy University . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TropEdition.h"
+
+@interface Tropolitan : NSObject <NSXMLParserDelegate>{
+	
+	NSMutableArray *editions;//Available Editions
+	TropEdition *current;
+	
+}
+
+@property(readonly, nonatomic, retain) NSMutableArray *editions;
+@property(readonly,nonatomic, retain) TropEdition *current;
+
+//Singleton Related:
++(Tropolitan*)instance;//reference the Trop instance
++(id)allocWithZone:(NSZone *)zone;
++(id)copyWithZone:(NSZone *)zone;
+-(NSUInteger)retainCount;
+-(void)release;
+-(id)autorelease;
+
+//Functional methods
+-(void) update;//Gets list of editions and sets current to most recent if current nil
+
+
+
+@end
