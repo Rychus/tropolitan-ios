74b022a0a0eb6e42d38f3940c10d1fddfcc03547
rmorris@stewonit.com <rmorris@stewonit.com@11dcd478-d5f8-5778-5ec9-787184ed5f8b>
2011-03-14 23:48:04 +0000

    article viw

---
+++ trunk/iPhone/ArticleView.h
@@ -0,0 +1,18 @@
+//
+//  ArticleView.h
+//  iPhone
+//
+//  Created by HSJC Student Account on 3/14/11.
+//  Copyright 2011 Troy University . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ArticleView : UIViewController {
+
+}
+
+
+
+@end
