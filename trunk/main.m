09ca6202eaf9e14aea9e79b592553017919d79e0
rmorris@stewonit.com <rmorris@stewonit.com@11dcd478-d5f8-5778-5ec9-787184ed5f8b>
2011-03-15 23:51:33 +0000

    Grabs Articles

--- trunk/main.m
+++ trunk/main.m
@@ -7,11 +7,20 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "Tropolitan.h"
 
 int main(int argc, char *argv[]) {
     
-    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
     int retVal = UIApplicationMain(argc, argv, nil, nil);
+    
+	//This is the location of your listing of editions
+	const NSString *tropArchiveURL = [[NSString alloc] initWithString:@"http://www.stewonit.com/trop/troparchive.xml"];
+	
+	Tropolitan* trop = [Tropolitan instance];
+	[trop update];
+	
+    
     [pool release];
     return retVal;
 }
