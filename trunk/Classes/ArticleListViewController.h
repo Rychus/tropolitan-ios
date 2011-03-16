648c23b71f8ed1c2c8c30ce23a5067a41b87b89a
rmorris@stewonit.com <rmorris@stewonit.com@11dcd478-d5f8-5778-5ec9-787184ed5f8b>
2011-03-16 00:45:41 +0000

    adjusted articlelistview

--- trunk/Classes/ArticleListViewController.h
+++ trunk/Classes/ArticleListViewController.h
@@ -1,15 +1,14 @@
 //
-//  ArticleListView.h
-//  Tropolitan-IOS
+//  ArticleListViewController.h
 //
 //  Created by HSJC Student Account on 3/15/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-
-@interface ArticleListView : UIViewController {
+@interface UITableViewController : UIViewController {
 
 }
 
---
+++ trunk/Classes/ArticleListViewController.m
@@ -0,0 +1,12 @@
+//
+//  ArticleListViewController.m
+//
+//  Created by HSJC Student Account on 3/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ArticleListViewController.h"
+
+@implementation UITableViewController
+
+@end
