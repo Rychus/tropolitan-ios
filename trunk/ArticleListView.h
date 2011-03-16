f62ee9cf7fcfa2f38146e36dff02a37c4f9e6530
rmorris@stewonit.com <rmorris@stewonit.com@11dcd478-d5f8-5778-5ec9-787184ed5f8b>
2011-03-16 00:40:31 +0000

    ArticleView template

---
+++ trunk/ArticleListView.h
@@ -0,0 +1,16 @@
+//
+//  ArticleListView.h
+//  Tropolitan-IOS
+//
+//  Created by HSJC Student Account on 3/15/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ArticleListView : UIViewController {
+
+}
+
+@end
