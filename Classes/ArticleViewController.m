6a64aef0ad5f184eabff82768b0712c50cfe9772
bshields1989 <bshields1989@gmail.com>
2011-03-29 23:56:45 +0000

    headline, byline added

--- Classes/ArticleViewController.m
+++ Classes/ArticleViewController.m
@@ -2,7 +2,7 @@
 //  ArticleViewController.m
 //  Trop
 //
-//  Created by mac on 3/27/11.
+//  Created by mac on 3/22/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
@@ -12,60 +12,55 @@
 
 
 @implementation ArticleViewController
+
 @synthesize body;
+@synthesize myView;
 
--(void) setArticle:(NSString *)headline
+- (void) setArticle:(NSString *)title
 {
-	bool found = false;
-	NSLog(@"Setting Article");
-	body.text = [body.text stringByAppendingString:@"setArticle"];
-	
-	for (Article *art in [Tropolitan instance].current.articles)
-	{
-		if(art.headline == headline)
-		{
-			found = true;
-			body.text = art.body;
-			
-			break;
-		}               
-	}
-	
-	if(!found)
-	{
-		NSLog(@"Failed to locate article");
-	}
-	 
-
+	articleTitle = [[NSString alloc] init];
+	articleTitle = title;
 }
 
 // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
 /*
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization.
-    }
-    return self;
-}
-*/
+ - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+ self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+ if (self) {
+ // Custom initialization.
+ }
+ return self;
+ }
+ */
 
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
+    [super viewDidLoad];
 	
-	[super viewDidLoad];
+	body = [[NSString alloc] init];
 	
+	for (Article *art in [Tropolitan instance].current.articles)
+	{
+		if(art.headline == articleTitle)
+		{
+			//body = art.body;
+			body = ([NSString stringWithFormat:@"%@\n%@\n%@\n\n%@", art.headline, art.byline, art.bylineTitle, art.body]);
+			//NSLog(@"body: %@", body);
+		}
+	}
+	NSLog(@"body: %@", body);
+	[myView setText:body];
 }
 
 
 /*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations.
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
+ // Override to allow orientations other than the default portrait orientation.
+ - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+ // Return YES for supported orientations.
+ return (interfaceOrientation == UIInterfaceOrientationPortrait);
+ }
+ */
 
 - (void)didReceiveMemoryWarning {
     // Releases the view if it doesn't have a superview.
@@ -76,7 +71,6 @@
 
 - (void)viewDidUnload {
     [super viewDidUnload];
-	
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
 }
@@ -84,9 +78,6 @@
 
 - (void)dealloc {
     [super dealloc];
-	[body release];
-	[headline release];
-	[byline release];
 }
 
 
