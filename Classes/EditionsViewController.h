c5bf002146e86af745fa1ce5253155ed069416ed
bshields1989 <bshields1989@gmail.com>
2011-03-29 22:46:59 +0000

    added EditionsView files

---
+++ Classes/EditionsViewController.h
@@ -0,0 +1,19 @@
+//
+//  EditionsViewController.h
+//  Trop
+//
+//  Created by mac on 3/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Tropolitan.h"
+
+@interface EditionsViewController : UITableViewController {
+
+	NSMutableArray *editionsList;
+}
+
+@property (nonatomic, retain) IBOutlet NSMutableArray *editionsList;
+
+@end
