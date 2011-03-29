417abbe6e0b4fdc269ca61973b7e489b0eaa64f9
jeibaansu@gmail.com <jeibaansu@gmail.com@11dcd478-d5f8-5778-5ec9-787184ed5f8b>
2011-03-29 23:28:29 +0000

    Working on background image for navigation bar

---
+++ trunk/Classes/UINavBarImage.m
@@ -0,0 +1,39 @@
+//
+//  UINavBarImage.m
+//  Trop
+//
+//  Created by HSJC Student Account on 3/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "UINavBarImage.h"
+
+
+static NSMutableDictionary *navigationBarImages = NULL;
+
+@implementation UINavigationBar(CustomImage)
+//Overrider to draw a custom image
+
++ (void)initImageDictionary
+{
+    if(navigationBarImages==NULL){
+        navigationBarImages=[[NSMutableDictionary alloc] init];
+    }   
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    NSString *imageName=[navigationBarImages objectForKey:[NSValue valueWithNonretainedObject: self]];
+    if (imageName==nil) {
+        imageName=@"banner.png";
+    }
+    UIImage *image = [UIImage imageNamed: imageName];
+    [image drawInRect:CGRectMake(0, 0, self.frame.size.width, self.frame.size.height)];
+}
+
+//Allow the setting of an image for the navigation bar
+- (void)setImage:(UIImage*)image
+{
+    [navigationBarImages setObject:image forKey:[NSValue valueWithNonretainedObject: self]];
+}
+@end
